9d0079face625518887ddbd4b3adb8a822ef8a2e
Patrick Abejar <wireless.patrick@gmail.com>
2018-04-30 15:01:10 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,11 @@
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 def valid_move?(board, index)
   if position_taken?(board, index)
     false
@@ -14,12 +22,4 @@ def position_taken?(board, index)
   elsif (board[index] == "" || board[index] == " " || board[index] == nil)
     false
   end
-end
-
-def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
